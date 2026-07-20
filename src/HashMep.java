950d48d73185125b8c03193a95507632b4398afb
sayan <sayanmajhi05@gmail.com>
2026-07-20 14:09:37 +0530

    Created basic HashMap,updated form -collison,resize

--- src/HashMep.java
+++ src/HashMep.java
@@ -1,38 +1,145 @@
-import java.util.HashMap;
+import java.util.Arrays;
+
 public class HashMep {
+
+    private int size = 0;
+
     static class Element {
         String key;
         String value;
+        Element next;
+
         Element(String key, String value) {
             this.key = key;
             this.value = value;
         }
     }
-    Element[] elements;
-    HashMep() {
+
+    private Element[] elements;
+
+    public HashMep() {
         elements = new Element[100];
     }
-public void put(String key, String value) {
-        Element element = new Element(key, value);
-        int hash = key.hashCode()%elements.length;
-        elements[hash] = element;
-}
-public String get(String key) {
-    Element element = elements[key.hashCode()%elements.length];
-    if (element.key.equals(key)) {
-        return element.value;
-    }else{
+
+    public int getSize() {
+        return size;
+    }
+
+    private int getIndex(String key) {
+        return (key.hashCode() & 0x7fffffff) % elements.length;
+    }
+
+    public void put(String key, String value) {
+
+        if (size >= elements.length) {
+            resize();
+        }
+        int index = getIndex(key);
+        if (elements[index] == null) {
+            elements[index] = new Element(key, value);
+            size++;
+            return;
+        }
+
+        Element curr = elements[index];
+
+        while (true) {
+
+            // Key already exists -> update value
+            if (curr.key.equals(key)) {
+                curr.value = value;
+                return;
+            }
+
+            if (curr.next == null)
+                break;
+
+            curr = curr.next;
+        }
+
+        // Collision -> append
+        curr.next = new Element(key, value);
+        size++;
+    }
+
+    public String get(String key) {
+
+        int index = getIndex(key);
+
+        Element curr = elements[index];
+
+        while (curr != null) {
+
+            if (curr.key.equals(key))
+                return curr.value;
+
+            curr = curr.next;
+        }
+
         return null;
     }
-}
-public static void main(String[] args) {
-        HashMep hm = new HashMep();
-        hm.put("a", "a");
-        hm.put("b", "b");
-        hm.put("c", "c");
-        hm.put("d", "d");
 
-        System.out.println(hm.get("a"));
-}
+    public void remove(String key) {
 
+        int index = getIndex(key);
+
+        Element curr = elements[index];
+        Element prev = null;
+
+        while (curr != null) {
+
+            if (curr.key.equals(key)) {
+
+                if (prev == null) {
+                    elements[index] = curr.next;
+                } else {
+                    prev.next = curr.next;
+                }
+
+                size--;
+                return;
+            }
+
+            prev = curr;
+            curr = curr.next;
+        }
+    }
+
+    private void resize() {
+
+        Element[] old = elements;
+
+        elements = new Element[old.length * 2];
+
+        size = 0;
+
+        for (Element head : old) {
+
+            while (head != null) {
+                put(head.key, head.value);
+                head = head.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMep map = new HashMep();
+
+        map.put("a", "Apple");
+        map.put("b", "Ball");
+        map.put("c", "Cat");
+
+        System.out.println(map.get("a"));
+
+        map.put("a", "Ant");
+
+        System.out.println(map.get("a"));
+
+        map.remove("a");
+
+        System.out.println(map.get("a"));
+
+        System.out.println("Size = " + map.getSize());
+    }
 }
